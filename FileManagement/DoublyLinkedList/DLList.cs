fbe23fab4634764bea6f4f4956a6deb8aad27bea
argyrides <alexandros.argyrides@gmail.com>
2022-10-30 02:02:38 +0300

    First Commit

---
+++ FileManagement/DoublyLinkedList/DLList.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagement.DoublyLinkedList
+{
+    internal class DLList<T>
+    {
+        public GenericNode<T>? Head { get; set; }
+        public GenericNode<T>? Tail { get; set; }
+
+        public DLList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public void InsertFirst(T t)
+        {
+            GenericNode<T> tmp = new GenericNode<T>();
+            tmp.Value = t;
+            tmp.Next = Head;
+            tmp.Prev = null;
+
+            tmp.Count++;
+
+            if (IsEmpty())
+            {
+                Tail = tmp;
+            }
+
+            Head = tmp;
+        }
+
+        public void InsertLast(T t)
+        {
+            if (Head == null)
+            {
+                InsertFirst(t);
+                return;
+            }
+
+            GenericNode<T> tmp = new();
+            tmp.Value = t;
+            tmp.Next = null;
+            tmp.Prev = Tail;
+
+            tmp.Count++;
+
+            Tail!.Next = tmp;
+            Tail = tmp;
+
+        }
+
+        public void Traverse(int count) 
+        { 
+            if (Head == null)
+            {
+                Console.WriteLine("Empty List");
+                return;
+            }
+
+            Console.WriteLine($"Total Chars: {count}");
+            for (GenericNode<T> node = Head; node != null; node = node.Next!)
+            {
+                Console.WriteLine($"Value: {node.Value}, Count: {node.Count}, Frequency: {((double)node.Count / count):P}");
+            }
+        }
+
+        public GenericNode<T>? GetNodePosition(T t)
+        {
+            GenericNode<T> tmp = Head!;
+            while (tmp != null)
+            {
+                if (tmp.Value!.Equals(t))
+                {
+                    return tmp;
+                }
+                tmp = tmp.Next!;
+            }
+
+            return null;
+        }
+
+        public void IncreaseCount(GenericNode<T> node)
+        {
+            node.Count++;
+        }
+
+        public void IncreaseCount(T t)
+        {
+            GetNodePosition(t)!.Count++;
+        }
+
+        public void SortByValAsc()
+        {
+            for (GenericNode<T> node = Head!; node != null; node = node.Next!)
+            {
+                T minVal = node.Value!;
+                GenericNode<T> minPos = node;
+
+                for (GenericNode<T> jNode = node; jNode != null; jNode = jNode.Next!)
+                {
+                    if (jNode.Value is char)
+                    {
+                        if (Convert.ToChar(jNode.Value) < Convert.ToChar(minVal))
+                        {
+                            minVal = jNode.Value;
+                            minPos = jNode;
+                        }
+                    }
+                }
+                Swap(node, minPos);
+            }
+        }
+
+
+        public void Swap(GenericNode<T> iNode, GenericNode<T> jNode) 
+        {
+            T tempVal = iNode.Value!;
+            int tempCount = iNode.Count;
+
+            iNode.Value = jNode.Value!;
+            iNode.Count = jNode.Count;
+
+            jNode.Value = tempVal;
+            jNode.Count = tempCount;
+        }
+
+        public void SortyByFrequencyDesc()
+        {
+            for (GenericNode<T> iNode = Head!; iNode != null; iNode = iNode.Next!)
+            {
+                int minVal = iNode.Count;
+                GenericNode<T> minPos = iNode;
+
+                for (GenericNode<T> jNode = iNode; jNode != null; jNode = jNode.Next!)
+                {
+                    if(jNode.Count > minVal)
+                    {
+                        minVal = jNode.Count;
+                        minPos = jNode;
+                    }
+                }
+                Swap(iNode, minPos);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+
+    }
+}
---
+++ FileManagement/Program.cs
@@ -0,0 +1,65 @@
+﻿using FileManagement.DoublyLinkedList;
+
+namespace FileManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DLList<char> FileList = new();
+            StreamReader reader;
+            int TotalCharCount = 0;
+            char ch;
+
+            string file = @"C:\tmp\file.txt";
+            GenericNode<char> node;
+            
+            try
+            {
+                reader = new StreamReader(file);
+
+                while (reader.Peek() >= 0)
+                {
+                    ch = (char)reader.Read();
+                    if ((Convert.ToInt32(ch) == 13) || (Convert.ToInt32(ch) == 10))
+                    {
+                        reader.Read();
+                        continue;
+                    }
+
+                    node = FileList.GetNodePosition(ch)!;
+                    if (node != null)
+                    {
+                        FileList.IncreaseCount(node);
+                    }
+                    else
+                    {
+                        FileList.InsertLast(ch);
+                    }
+
+                    TotalCharCount++;
+                }
+                reader.Close();
+                Console.WriteLine("Traverse...");
+                FileList.Traverse(TotalCharCount);
+
+                Console.WriteLine("Traverse by char asc");
+                FileList.SortByValAsc();
+                FileList.Traverse(TotalCharCount);
+
+                Console.WriteLine("Traverse by char frequency");
+                FileList.SortyByFrequencyDesc();
+                FileList.Traverse(TotalCharCount);
+            } 
+            catch (FileNotFoundException e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+            catch (IOException e2)
+            {
+                Console.WriteLine(e2.Message);
+            }
+
+        }
+    }
+}
